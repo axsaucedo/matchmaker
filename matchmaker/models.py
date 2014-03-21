61fac0a39450356c80aa987c9b21849f9f3668f3
Alejandro Saucedo <axsauze@gmail.com>
2014-03-22 03:29:11 +0400

    3am push

--- matchmaker/models.py
+++ matchmaker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    profile_photo = models.ImageField(upload_to='profiles/')
+    profile_photo = models.ImageField(upload_to='profiles/', blank=True, null=True)
     phone = models.CharField(max_length=13)
 
     def image_tag(self):
--- matchmaker/views.py
+++ matchmaker/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from models import MatchUp, UserProfile, User
-#from twilio.rest import TwilioRestClient
+from twilio.rest import TwilioRestClient
+import random
 
 
-## Twilio Settings
-#account = "AC8f68f68ffac59fd5afc1a3317b1ffdf8"
-#token = "5a556d4a9acf96753850c39111646ca4"
-#client = TwilioRestClient(account, token)
-#fromnumber = "+441279702159"
+# Twilio Settings
+account = "AC8f68f68ffac59fd5afc1a3317b1ffdf8"
+token = "5a556d4a9acf96753850c39111646ca4"
+client = TwilioRestClient(account, token)
+fromnumber = "+447903518974"
 
 def matchUpRequest(request):
     context = RequestContext(request)
@@ -30,22 +31,27 @@ def matchUpRequest(request):
                 lat = post['lat']
                 long = post['long']
 
-#                him = None
-#                her = None
-#                try:
-#                    him = UserProfile.objects.get(phone=his_phone)
-#                except:
-#                    pass
-#                try:
-#                    her = UserProfile.objects.get(phone=her_phone)
-#                except:
-#                    pass
-#
-#                matchup = MatchUp(match_maker=request.user, him=him, her=her, his_phone=his_phone, her_phone=her_phone)
-#                matchup.save()
-#
-#                message = "Your friend " + request.user.first_name + " " + request.user.last_name + " has set you up on a blind date! Please access http://localhost:8000/matchups/" + str(matchup.id) + " to check it!"
-#
+                him_pass = random.randrange(9999)
+                her_pass = random.randrange(9999)
+                try:
+                    him = UserProfile.objects.get(phone=his_phone)
+                except:
+                    u_him = User.objects.create(username=his_phone.replace('+',''), password=him_pass)
+                    him = UserProfile.objects.create(user=u_him, phone=his_phone)
+                try:
+                    her = UserProfile.objects.get(phone=her_phone)
+                except:
+                    u_her = User.objects.create(username=her_phone.replace('+',''), password=her_pass)
+                    her = UserProfile.objects.create(user=u_her, phone=her_phone)
+
+                matchup = MatchUp(match_maker=request.user, him=him.user, her=her.user, his_phone=his_phone, her_phone=her_phone)
+                matchup.save()
+
+                print him
+                print her
+
+                message = "Your friend " + request.user.first_name + " " + request.user.last_name + " has set you up on a blind date! Please access http://localhost:8000/matchups/" + str(matchup.id) + " to check it!"
+
 #                his_sms = client.sms.messages.create(body=message,
 #                                                    to=her_phone,
 #                                                    from_=fromnumber)
@@ -53,6 +59,8 @@ def matchUpRequest(request):
 #                her_sms = client.sms.messages.create(body=message,
 #                                                    to=his_phone,
 #                                                    from_=fromnumber)
+
+                print "Emails sent"
 
                 active = matchup
 
